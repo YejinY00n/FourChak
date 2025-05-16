4847e04e53d88a61a40762a92641302be9b26074
todok0317 <todok0317@naver.com>
2025-05-16 19:13:09 +0900

    Feat : Store 엔티티 추가, Store 기능 구현 (가게 등록, 가게 조회, 가게 정보수정, 가게 폐업)

---
+++ src/main/java/org/example/fourchak/store/repository/StoreRepository.java
@@ -0,0 +1,13 @@
+package org.example.fourchak.store.repository;
+
+import org.example.fourchak.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StoreRepository extends JpaRepository<Store, Long> {
+
+    default Store findStoreByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 가게입니다."));
+    }
+
+
+}

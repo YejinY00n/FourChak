79d881ee3afe63c27d43d4e728dad8a01707108a
todok0317 <todok0317@naver.com>
2025-05-19 12:18:52 +0900

    Feat : SearchKeyword 레포지토리 설정

---
+++ src/main/java/org/example/fourchak/domain/search/repository/SearchKeywordRepository.java
@@ -0,0 +1,19 @@
+package org.example.fourchak.domain.search.repository;
+
+import java.util.List;
+import java.util.Optional;
+import org.example.fourchak.domain.search.entity.SearchKeyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchKeywordRepository extends JpaRepository<SearchKeyword, Long> {
+
+    Optional<SearchKeyword> findByKeyword(String keyword);
+
+    @Query("SELECT s F from SearchKeyword s ORDER BY s.count DESC LIMIT 10")
+    List<SearchKeyword> findTop10ByOrderByCountDesc();
+
+
+}

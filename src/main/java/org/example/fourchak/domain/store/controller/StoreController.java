6ec256c65d124097295bc7a7e849a258fa02f766
todok0317 <todok0317@naver.com>
2025-05-19 11:06:14 +0900

    Feat : Store 오류 수중 및 user 정보 가져오는 @AuthenticationPrincipal CustomUserPrincipal user 사용.

--- src/main/java/org/example/fourchak/domain/store/controller/StoreController.java
+++ src/main/java/org/example/fourchak/domain/store/controller/StoreController.java
@@ -3,12 +3,15 @@ package org.example.fourchak.domain.store.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.fourchak.common.ResponseMessage;
+import org.example.fourchak.config.security.CustomUserPrincipal;
 import org.example.fourchak.domain.store.dto.request.StoreRequestDto;
 import org.example.fourchak.domain.store.dto.response.StoreResponseDto;
 import org.example.fourchak.domain.store.service.StoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +29,9 @@ public class StoreController {
     // 가게 등록
     @PostMapping
     public ResponseEntity<ResponseMessage<StoreResponseDto>> saveStore(
-        @Valid @RequestBody StoreRequestDto requestDto) {
-        StoreResponseDto responseDto = storeService.saveStore(requestDto, userId);
+        @Valid @RequestBody StoreRequestDto requestDto,
+        @AuthenticationPrincipal CustomUserPrincipal user) {
+        StoreResponseDto responseDto = storeService.saveStore(requestDto, user.getId());
 
         ResponseMessage<StoreResponseDto> responseMessage = ResponseMessage.<StoreResponseDto>builder()
             .statusCode(HttpStatus.CREATED.value())
@@ -39,7 +43,7 @@ public class StoreController {
     }
 
     // 가게 단건조회
-    @PostMapping("/{storeId}")
+    @GetMapping("/{storeId}")
     public ResponseEntity<ResponseMessage<StoreResponseDto>> findStoreById(
         @PathVariable Long storeId) {
         StoreResponseDto responseDto = storeService.findStoreById(storeId);
@@ -56,8 +60,9 @@ public class StoreController {
     // 가게 정보수정
     @PatchMapping("/{storeId}")
     public ResponseEntity<ResponseMessage<StoreResponseDto>> updateStore(@PathVariable Long storeId,
-        @RequestBody StoreRequestDto requestDto) {
-        StoreResponseDto responseDto = storeService.updateStore(storeId, requestDto);
+        @RequestBody StoreRequestDto requestDto,
+        @AuthenticationPrincipal CustomUserPrincipal user) {
+        StoreResponseDto responseDto = storeService.updateStore(user.getId(), storeId, requestDto);
 
         ResponseMessage<StoreResponseDto> responseMessage = ResponseMessage.<StoreResponseDto>builder()
             .statusCode(HttpStatus.OK.value())
@@ -70,12 +75,13 @@ public class StoreController {
 
     // 가게 폐업하기
     @DeleteMapping("/{storeId}")
-    public ResponseEntity<ResponseMessage<Void>> deleteStore(@PathVariable Long storeId) {
-        storeService.deleteStore(storeId);
+    public ResponseEntity<ResponseMessage<Void>> deleteStore(@PathVariable Long storeId,
+        @AuthenticationPrincipal CustomUserPrincipal user) {
+        storeService.deleteStore(storeId, user.getId());
 
         ResponseMessage<Void> responseMessage = ResponseMessage.<Void>builder()
             .statusCode(HttpStatus.OK.value())
-            .message("가게 정보가 수정되었습니다.")
+            .message("가게가 폐업처리 되었습니다.")
             .build();
 
         return ResponseEntity.status(HttpStatus.OK).body(responseMessage);

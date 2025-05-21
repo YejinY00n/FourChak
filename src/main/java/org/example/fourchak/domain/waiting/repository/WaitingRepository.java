d8b3417f02d3901e8522c32acc8f8cb573963889
jypLord <jygold2n@naver.com>
2025-05-21 20:02:41 +0900

    fix: 사용하지 않는 메서드 삭제

--- src/main/java/org/example/fourchak/domain/waiting/repository/WaitingRepository.java
+++ src/main/java/org/example/fourchak/domain/waiting/repository/WaitingRepository.java
@@ -4,7 +4,6 @@ import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.persistence.LockModeType;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import org.example.fourchak.domain.waiting.entity.Waiting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
@@ -22,10 +21,5 @@ public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
     void deleteByReservationTimeBefore(LocalDateTime reservationTime);
 
-    @Query("SELECT w FROM Waiting w JOIN Reservation r ON w.user.id")
-    Optional<Waiting> findByReservationId(@Param("reservationId") Long reservationId);
-
     boolean existsByStoreIdAndReservationTime(Long storeId, LocalDateTime reservationTime);
-
-    int countByStoreIdAndReservationTime(Long storeId, LocalDateTime reservationTime);
 }

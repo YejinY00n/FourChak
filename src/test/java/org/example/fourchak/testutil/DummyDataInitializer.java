66733fb847f8e89f187b987a96a928fbc212b4fa
soonch67890 <soonch67890@gmail.com>
2025-05-21 16:29:49 +0900

    Feat : UserRepositoryIndexTest 테스트코드 작성
     Remove : UserControllerTest 삭제
    
     이유 : DB에 관련되 테스트는 레파지토리를 테스트 해야함

--- src/test/java/org/example/fourchak/testutil/DummyDataInitializer.java
+++ src/test/java/org/example/fourchak/testutil/DummyDataInitializer.java
@@ -12,8 +12,8 @@ public class DummyDataInitializer {
     private final DummyUserDataService dummyUserDataService;
 
     // 사용 할려면 아래 주석만 제거 하세요.
-    //@PostConstruct
+    // @PostConstruct
     public void init() {
-        dummyUserDataService.insertDummyUsers(1_000); // 100만개
+        dummyUserDataService.insertDummyUsers(10_000);
     }
 }

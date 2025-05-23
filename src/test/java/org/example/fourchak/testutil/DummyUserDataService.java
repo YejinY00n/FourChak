74360183a3e738a1b6a816c2d46cc8db6294bddb
soonch67890 <soonch67890@gmail.com>
2025-05-23 12:58:04 +0900

    Fix : 테스트 config가 없으면 충돌이 예측되어 복구함

--- src/test/java/org/example/fourchak/testutil/DummyUserDataService.java
+++ src/test/java/org/example/fourchak/testutil/DummyUserDataService.java
@@ -6,13 +6,14 @@ import java.util.List;
 import java.util.Random;
 import java.util.Set;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.example.fourchak.config.TestSecurityConfig;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Import(TestSecurityConfig.class)
 @Service
 @Profile("test")
 @RequiredArgsConstructor
@@ -76,10 +77,5 @@ public class DummyUserDataService {
         }
 
         return sb.toString();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }

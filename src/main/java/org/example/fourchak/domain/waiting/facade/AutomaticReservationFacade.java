bdf72237ee8046a5f561b3186a6c77224a4d9ae1
jypLord <jygold2n@naver.com>
2025-05-26 07:19:51 +0900

    Update AutomaticReservationFacade.java

--- src/main/java/org/example/fourchak/domain/waiting/facade/AutomaticReservationFacade.java
+++ src/main/java/org/example/fourchak/domain/waiting/facade/AutomaticReservationFacade.java
@@ -20,7 +20,7 @@ public class AutomaticReservationFacade {
     private final WaitingService waitingService;
 
     //store 에서 총 자리수 가져와서 연산하기
-    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void doIt(DeleteReservationEvent event) {
         int availableSeat = storeService.findStoreById(event.getStoreId()).getSeatCount()
